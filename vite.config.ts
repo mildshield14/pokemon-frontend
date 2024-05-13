fd98747a64af8e93e66c3f98018e47c11b9b2ff3
Vennila Sooben <80240232+mildshield14@users.noreply.github.com>
2024-05-13 12:21:34 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
-    base: "/pokemon-game-frontend/",
+    base: "/pokemon-frontend/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
